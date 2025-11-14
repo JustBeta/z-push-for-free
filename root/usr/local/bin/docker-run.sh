f381c3e56169b1911ff7f511ce7d585622e6e3ea
justbeta <jbarone@free.fr>
2025-11-14 10:41:43 +0100

    Refactor docker-run.sh to use multiple temp files

--- root/usr/local/bin/docker-run.sh
+++ root/usr/local/bin/docker-run.sh
@@ -43,7 +43,8 @@ sed -e "s/\/\/ define('ZPUSH_HOST', 'zpush.example.com')/define('ZPUSH_HOST', '"
 
 # ajout d'une fonction pour mettre le bon domaine de messagerie
 fichier="/usr/local/lib/z-push/autodiscover/autodiscover.php.dist"
-temp="/usr/local/lib/z-push/autodiscover/autodiscover.php.tmp"
+temp1="/usr/local/lib/z-push/autodiscover/autodiscover.php.tmp1"
+temp2="/usr/local/lib/z-push/autodiscover/autodiscover.php.tmp2"
 insertion=$(cat <<'EOF'
 $local_domain = 'DOMAIN_PERSO';
 $provider_domain = 'DOMAIN_ISP';
@@ -65,9 +66,9 @@ EOF
 # Traitement ligne par ligne
 while IFS= read -r ligne; do
     if [[ "$ligne" == "require_once '../vendor/autoload.php';" ]]; then
-        echo "$insertion" >> "$temp"
+        echo "$insertion" >> "$temp1"
     fi
-    echo "$ligne" >> "$temp"
+    echo "$ligne" >> "$temp1"
 done < "$fichier"
 
 insertion=$(cat <<'EOF'
@@ -83,14 +84,15 @@ EOF
 # Traitement ligne par ligne
 while IFS= read -r ligne; do
     if [[ "$ligne" == '        $xml = simplexml_load_string($input);' ]]; then
-        echo "$insertion" >> "$temp"
+        echo "$insertion" >> "$temp2"
     fi
-    echo "$ligne" >> "$temp"
-done < "$fichier"
+    echo "$ligne" >> "$temp2"
+done < "$temp1"
 
 sed -e "s/local_domain = 'DOMAIN_PERSO'/local_domain = $DOMAIN_PERSO/" \
-    -e "s|provider_domain = 'DOMAIN_ISP'|provider_domain = $DOMAIN_ISP|" /usr/local/lib/z-push/autodiscover/autodiscover.php.tmp > /usr/local/lib/z-push/autodiscover/autodiscover.php
-rm /usr/local/lib/z-push/autodiscover/autodiscover.php.tmp
+    -e "s|provider_domain = 'DOMAIN_ISP'|provider_domain = $DOMAIN_ISP|" /usr/local/lib/z-push/autodiscover/autodiscover.php.tmp2 > /usr/local/lib/z-push/autodiscover/autodiscover.php
+rm /usr/local/lib/z-push/autodiscover/autodiscover.php.tmp1
+rm /usr/local/lib/z-push/autodiscover/autodiscover.php.tmp2
 
 # si un fichier de config est present dans /config, alors on utilise celui la.
 [ -f "/config/config.php" ] && cat /config/config.php >> /usr/local/lib/z-push/config.php
